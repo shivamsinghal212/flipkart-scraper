9ed840dd71d2e5a8e2706921a7369f1e3e930324
shivam singhal <shivam.singhal212@gmail.com>
2018-05-24 13:25:38 +0530

    Flipkart-scraper V1.1: Now handles any type of screen structure.

--- flipkart_root.py
+++ flipkart_root.py
@@ -15,6 +15,12 @@ class Scrapper:
                           'specs': 'vFw0gD',
                           'price': '_1vC4OE _2rQ-NK',
                           'mrp': '_3auQ3N _2GcJzG', }
+    BOX_PRODUCT_CLASS_DICT = {'name': '_2cLu-l',  # <a> class
+                              'rating': 'hGSR34 _2beYZw',
+                              'rating2': 'hGSR34 _1x2VEC',
+                              'rating3': 'hGSR34 _1nLEql',
+                              'specs': '_1rcHFq',   # <div> class
+                              'price': '_1vC4OE', }
 
     def __init__(self, searchterm):
         self.searchterm = searchterm
@@ -57,14 +63,13 @@ class Scrapper:
                 end = raw_results.index('results')
                 no_of_results = int(raw_results[start + 3:end - 1].replace(',', ''))
                 logging.info('Number of results for ' + self.searchterm + ':' + str(no_of_results))
-                if no_of_results > 1000:
+                if no_of_results > 10000:
                     print('Too many' + '(' + str(no_of_results) + ')results for ' + self.searchterm + '.\
  Please extend your search term.')
                     print('Do you still want to continue, it will take a lot of time.(Y/N)')
                     choice = input()
                     if choice == 'Y' or choice == 'y':
-                        # TODO: Handle different screen format
-                        return self.handle_different_screen_format()
+                        return self.get_max_page(response)
                     elif choice == 'N' or choice == 'n':
                         exit()
                     else:
@@ -74,7 +79,6 @@ class Scrapper:
                     print('No of results: ', no_of_results)
                     return self.get_max_page(response)
         except AttributeError:
-            # TODO: handle different screen format
             logging.error("screen format different for this search result, cant continue" + self.searchterm)
             return self.handle_different_screen_format()
 
@@ -86,11 +90,10 @@ class Scrapper:
             raw_results = soup.find('div', {'class': klass}).select_one('span').get_text()
             start = raw_results.index('of')
             no_of_pages = int(raw_results[start + 3:].replace(' ', ''))
-            return self.create_page_urls(no_of_pages)
         except AttributeError:
             no_of_pages = 1
             logging.info('Only first page found')
-            return self.create_page_urls(no_of_pages)
+        return self.create_page_urls(no_of_pages)
 
     def create_page_urls(self, no_of_pages):
         pages_url_list = list()
@@ -122,10 +125,77 @@ class Scrapper:
                     continue
                 break
         if len(valid_page_url_list) is not None:
-            return self.get_product_info(valid_page_url_list)
+            return self.check_diplay_type(valid_page_url_list)
         else:
             print('No valid url found, exiting...')
             exit()
+
+    def check_diplay_type(self, valid_page_url_list):
+        # class = '_1HmYoV _35HD7C col-10-12' --> box format
+        # _1HmYoV hCUpcT
+
+        item = valid_page_url_list[0]
+        response = requests.get(item)
+        raw_html = response.content
+        soup = Bs(raw_html, 'html.parser')
+        try:
+            for var in soup.find_all("div", class_='bhgxx2 col-12-12'):
+                if var.find('a', {'class': '_2cLu-l'}) is not None:
+                    logging.info('Box type screen structure found')
+                    return self.get_product_info_box(valid_page_url_list)
+                elif var.find('div', {'class': self.PRODUCT_CLASS_DICT['name']}) is not None:
+                    return self.get_product_info(valid_page_url_list)
+                else:
+                    logging.error('screen type cannot be recognized')
+        except AttributeError:
+            logging.error('Wrong class name in check_display_type()')
+
+    def get_product_info_box(self, valid_page_url_list):
+        raw_name_list = list()
+        raw_rating_list = list()
+        raw_specs_list = list()
+        raw_price_list = list()
+        for item in valid_page_url_list:
+            response = requests.get(item)
+            raw_html = response.content
+            soup = Bs(raw_html, 'html.parser')
+            try:
+                for var in soup.find_all("div", class_='bhgxx2 col-12-12'):
+                    if var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating']}) is not None:
+                        rating = var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating']}).get_text()[:-2]
+                        raw_rating_list.append(float(rating))
+                    elif var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating2']}) is not None:
+                        rating = var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating2']}).get_text()[:-2]
+                        raw_rating_list.append(float(rating))
+                    elif var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating3']}) is not None:
+                        rating = var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['rating3']}).get_text()[:-2]
+                        raw_rating_list.append(float(rating))
+                    else:
+                        rating = 0
+                        raw_rating_list.append(rating)
+                    if var.find('a', {'class': self.BOX_PRODUCT_CLASS_DICT['name']}) is None:
+                        raw_name_list.append(None)
+                    else:
+                        name = var.find('a', {'class': self.BOX_PRODUCT_CLASS_DICT['name']}).get_text()
+                        raw_name_list.append(name)
+                    if var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['specs']}) is None:
+                        raw_specs_list.append(None)
+                    else:
+                        specs = var.find('div', {'class': self.BOX_PRODUCT_CLASS_DICT['specs']}).get_text()
+                        raw_specs_list.append(specs)
+                    if var.find("div", class_=self.BOX_PRODUCT_CLASS_DICT['price']) is None:
+                        raw_price_list.append(None)
+                    else:
+                        price = var.find("div", class_=self.BOX_PRODUCT_CLASS_DICT['price']).get_text()[1:].replace(',', '')
+                        raw_price_list.append(int(price))
+                print('Scraping...please wait...')
+            except AttributeError:
+                print('Class name is different')
+        df1 = df({'NAME': raw_name_list, 'RATING': raw_rating_list, 'SPECS': raw_specs_list, 'PRICE': raw_price_list})
+        df1 = df1.dropna()
+        print('No of valid products fetched: ' + str(df1.shape[0]))
+        print('Thank-you for using Flipkart-Scraper.')
+        return df1
 
     def get_product_info(self, valid_page_url_list):
         raw_name_list = list()
